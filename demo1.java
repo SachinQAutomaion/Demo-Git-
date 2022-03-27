d3e00c870b91d75343dbd3a61568bd44e1a95f3b
sachin <navalesachin46@gmail.com>
2022-03-27 10:02:35 +0530

    adding demo1.java

--- demo1.java
+++ demo1.java
@@ -4,6 +4,7 @@ public class demo1
 {
     public static void main(String[] args) {
         System.out.println("Sachin demo git and github");
+        System.out.println("And git github");
     }
 
 
